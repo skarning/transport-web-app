3e83e7993f40bdba9e6b10c560d0e2fc989682e5
skarning <sivertskarning@hotmail.com>
2020-05-02 11:19:59 +0200

    Finish sql alchemy models

--- venv/src/transport/models/TransportationWorker.py
+++ venv/src/transport/models/TransportationWorker.py
@@ -2,10 +2,13 @@ from transport import db
 
 
 class TransportationWorker(db.Model):
-
+    __tablename__ = "TransportationWorkers"
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(40), unique=False, nullable=False)
     birthday = db.Column(db.String(10))
     job_title = db.Column(db.String(60))
     email = db.Column(db.String(60), unique=True, nullable=False)
-    phone_number = db.Columns(db.String(12))
+    phone_number = db.Column(db.String(12))
+    # One to Many relationship with mission
+    missions = db.relationship(
+        "Mission", backref="TransportationWorkers", lazy=True)
